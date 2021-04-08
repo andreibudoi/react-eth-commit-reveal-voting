71c8aa1013b8757069cfb648996d5e5df6721b6e
andreibudoi <andreibudoi.abc@gmail.com>
2021-04-08 23:26:07 +0300

    Add function for calling contract and pass props to panels

--- client/src/Containers/Poll/Poll.js
+++ client/src/Containers/Poll/Poll.js
@@ -11,6 +11,7 @@ import {
   Text
 } from "rimble-ui";
 import { Owner, Info, Voter } from "./PollBoards";
+import Web3 from "web3";
 
 const Poll = ({ drizzleState, drizzle, poll }) => {
   const contract = drizzle.contracts[poll];
@@ -32,37 +33,123 @@ const Poll = ({ drizzleState, drizzle, poll }) => {
       const totalVotersKey = contract.methods["totalVoters"].cacheCall();
       const totalVotesKey = contract.methods["totalVotes"].cacheCall();
       const choicesKey = await contract.methods["getChoices"].cacheCall();
+      const voterKey = await contract.methods["voters"].cacheCall(
+        drizzleState.activeAccount.account
+      );
+
       setKeys({
         pollDetailsKey,
         pollOwnerKey,
         pollStateKey,
         totalVotersKey,
         totalVotesKey,
-        choicesKey
+        choicesKey,
+        voterKey
       });
     };
     readPoll();
   }, []);
 
+  useEffect(() => {
+    const updateVoterKey = async () => {
+      const voterKey = await contract.methods["voters"].cacheCall(
+        drizzleState.activeAccount.account
+      );
+      setKeys(keys => ({ ...keys, voterKey }));
+    };
+    updateVoterKey();
+  }, [contract, drizzleState, setKeys]);
+
   const pollDetails = contractState.pollDetails[keys.pollDetailsKey];
   const pollOwner = contractState.pollOwner[keys.pollOwnerKey];
   const pollState = contractState.pollState[keys.pollStateKey];
-  const totalVoters = contractState.pollState[keys.totalVotersKey];
-  const totalVotes = contractState.pollState[keys.totalVotesKey];
-  const choices = contractState.pollState[keys.choicesKey];
+  const totalVoters = contractState.totalVoters[keys.totalVotersKey];
+  const totalVotes = contractState.totalVotes[keys.totalVotesKey];
+  const choices = contractState.getChoices[keys.choicesKey];
+  const voter = contractState.voters[keys.voterKey];
+
+  // Owner fns
+  const addChoices = choices => {
+    contract.methods["addChoices"].cacheSend(choices, {
+      from: drizzleState.activeAccount.account
+    });
+  };
+
+  const addVoters = voters => {
+    // we need to format out array so that solidity can read it
+    // format is [["address", "name"], [...], ...]
+    const formattedVoters = voters.map(voter => [voter.address, voter.name]);
+    contract.methods["addVoters"].cacheSend(formattedVoters, {
+      from: drizzleState.activeAccount.account
+    });
+  };
+
+  const startVote = () => {
+    contract.methods["startVote"].cacheSend({
+      from: drizzleState.activeAccount.account
+    });
+  };
+
+  const startReveal = () => {
+    contract.methods["startReveal"].cacheSend({
+      from: drizzleState.activeAccount.account
+    });
+  };
+
+  const endVote = () => {
+    contract.methods["endVote"].cacheSend({
+      from: drizzleState.activeAccount.account
+    });
+  };
+
+  // Voter fns
+  const commitVote = (choiceIdx, password) => {
+    const vote = Web3.utils.keccak256(choiceIdx + "-" + password);
+    contract.methods["commitVote"].cacheSend(vote, {
+      from: drizzleState.activeAccount.account
+    });
+  };
+
+  const revealVote = (choiceIdx, password) => {
+    const vote = Web3.utils.keccak256(choiceIdx + "-" + password);
+    contract.methods["revealVote"].cacheSend(choiceIdx, vote, {
+      from: drizzleState.activeAccount.account
+    });
+  };
 
   return pollDetails &&
     pollOwner &&
     pollState &&
     totalVoters &&
     totalVotes &&
-    choices ? (
+    choices &&
+    voter ? (
     <>
       <Info
-        info={{ pollDetails, pollOwner, pollState, totalVoters, totalVotes, choices }}
+        data={{
+          pollDetails,
+          pollOwner,
+          pollState,
+          totalVoters,
+          totalVotes,
+          choices
+        }}
+        user={drizzleState.activeAccount.account}
       />
-      <Owner />
-      <Voter />
+      {pollOwner.value.toLowerCase() === drizzleState.activeAccount.account && (
+        <Owner
+          data={{ pollOwner, pollState }}
+          functions={{ addChoices, addVoters, startVote, startReveal, endVote }}
+          user={drizzleState.activeAccount.account}
+        />
+      )}
+      {voter.value.name && (
+        <Voter
+          data={{ pollOwner, pollState, choices }}
+          functions={{ commitVote, revealVote }}
+          user={drizzleState.activeAccount.account}
+        />
+      )}
     </>
   ) : (
     <div>Loading poll info...</div>
